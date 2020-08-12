99f8ee2df7272052b63c66409e3f6aaf2108c135
Rafal Piekara <rafal@piekara.me>
2020-08-12 12:23:02 +0200

    Remove files

--- js/webapp/src/policies/MembershipPolicies.js
+++
@@ -1,3 +0,0 @@
-export const isValidForMembership = (user) => {
-  //some logic
-}
--- js/webapp/src/policies/ValidForMembership.js
+++
@@ -1,9 +0,0 @@
-export default class ValidForMembership {
-  constructor(user) {
-    this.user = user
-  }
-
-  apply() {
-    this.user.isActive && !this.user.isAdmin
-  }
-}
